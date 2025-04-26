a5d68ed703d0fe186bdada1ff9242b8c7dcbcc3e
Afaf <belafef9@gmail.com>
2025-04-26 20:32:50 +0200

    first commitportfolio

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let btnClose= document.getElementById('close');
 let container=document.getElementById('drop');
 
 btnClose.onclick=function(){
-    /*container.classList.add('hide');
     
-    btnOpen.classList.remove('hide');*/
     container.style.display="none";
 }
 btnOpen.onclick=function(){
